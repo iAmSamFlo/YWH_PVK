419b8f712f30a81b63831703943ef95f410914c3
adamnj <adamnj@kth.se>
2024-02-15 15:45:16 +0100

    add drawing

--- public/index.js
+++ public/index.js
@@ -2,15 +2,18 @@ let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
+  const { DrawingManager } = await google.maps.importLibrary("drawing");
 
   map = new Map(document.getElementById("map"), {
     center: { lat: 59, lng: 18 },
     zoom: 8,
     mapTypeId: "roadmap",
     streetViewControl: false,
-    
   });
+
+  var drawingManager = new DrawingManager();
+  drawingManager.setMap(map);
+  drawingManager.drawingControl = true;
 }
 
 initMap();
-
