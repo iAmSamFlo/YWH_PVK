0b850a8c3faf69cd8a7d3b1fa56e038e470c395e
Linus Markström <lmark@kth.se>
2024-04-10 14:10:56 +0200

    Fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public', { 'Content-Type': 'application/javascript' }));
 
 // The secret value is global for snappy reuse.
-let secret;
+let secret = process.env.GoogleMapsAPI;
 
 app.get('/get-secret', async(req, res) =>{
     if(!secret) {
@@ -29,26 +29,6 @@ app.get('/get-secret', async(req, res) =>{
         }
     }
     res.send(secret);
-});
-
-app.get('/check', async (req, res) => {
- if (!secret) {
-   try {
-     // Latency on getting the secret happens on the first request.
-     // If for some reason it fails, it will retry next time.
-     secret = await getSecret();
-   }
-   catch (e) {
-     console.error(e);
-   }
- }
- const target = secret || 'Ministry of Magic';
- res.send(`You serve the ${target}!`);
-});
-
-app.get('/', (req, res) => {
-  const msg = "<Secret cehcker, use: > '/check'";
-  res.render('index', { secret: secret});
 });
 
 // Handle the button click data
@@ -72,32 +52,4 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
-// SECRET_NAME is the resource ID of the secret, passed in by environment variable.
-// Format: projects/PROJECT_ID/secrets/SECRET_ID/versions/VERSION
-const {SECRET_NAME} = process.env;
- 
-if (!SECRET_NAME) {
-  console.log('Must set "SECRET_NAME" environment variable');
-  process.exit(1);
-}
 
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-// Secret Manager Client is global for snappy reuse.
-let client;
-
-// Load the secret from Secret Manager.
-async function getSecret() {
-  if (!client) client = new SecretManagerServiceClient();
-  if (SECRET_NAME && SECRET_NAME !== "dev") {
-    try {
-      const [version] = await client.accessSecretVersion({
-        name: SECRET_NAME,
-      });
-      return version.payload.data.toString('utf8');
-    }
-    catch (e) {
-      console.error(`error: could not retrieve secret: ${e}`);
-      return
-    }
-  }
-}
