3b9f4f8b15418057bc43756bd4c0185ce652c125
Linus Markström <lmark@kth.se>
2024-04-18 10:20:41 +0200

    test 3921

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,47 +13,27 @@
 // limitations under the License.
 
 'use strict';
-// [START cloud_sql_postgres_knex_connect_connector]
+// [START cloud_sql_postgres_knex_connect_unix]
 const Knex = require('knex');
-const {Connector} = require('@google-cloud/cloud-sql-connector');
 
-// In case the PRIVATE_IP environment variable is defined then we set
-// the ipType=PRIVATE for the new connector instance, otherwise defaults
-// to public ip type.
-const getIpType = () =>
-  process.env.PRIVATE_IP === '1' || process.env.PRIVATE_IP === 'true'
-    ? 'PRIVATE'
-    : 'PUBLIC';
-
-// connectWithConnector initializes connection pool for a Cloud SQL instance
-// of Postgres using the Cloud SQL Node.js Connector.
-const connectWithConnector = async config => {
+// createUnixSocketPool initializes a Unix socket connection pool for
+// a Cloud SQL instance of Postgres.
+const createUnixSocketPool = async config => {
   // Note: Saving credentials in environment variables is convenient, but not
   // secure - consider a more secure solution such as
   // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
   // keep secrets safe.
-  const connector = new Connector();
-  const clientOpts = await connector.getOptions({
-    //instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
-    instanceConnectionName: "stockholm-safety-map:europe-west1:ywhinstancepostgressql1",
-    ipType: getIpType(),
-  });
-  const dbConfig = {
+  return Knex({
     client: 'pg',
     connection: {
-      ...clientOpts,
-      //user: process.env.DB_USER, // e.g. 'my-user'
-      user: "postgres",
-      //password: process.env.DB_PASS, // e.g. 'my-user-password'
-      password: "FindTheWayHome1337", // e.g. 'my-user-password'
-      //database: process.env.DB_NAME, // e.g. 'my-database'
-      database: "db-ywh", // e.g. 'my-database'
+      user: process.env.DB_USER, // e.g. 'my-user'
+      password: process.env.DB_PASS, // e.g. 'my-user-password'
+      database: process.env.DB_NAME, // e.g. 'my-database'
+      host: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
     },
     // ... Specify additional properties here.
     ...config,
-  };
-  // Establish a connection to the database.
-  return Knex(dbConfig);
+  });
 };
-// [END cloud_sql_postgres_knex_connect_connector]
-module.exports = connectWithConnector;
+// [END cloud_sql_postgres_knex_connect_unix]
+module.exports = createUnixSocketPool;
