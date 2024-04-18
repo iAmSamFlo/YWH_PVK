c2f6d02a17a76770f20fa2e8c82ec3c4c9667a82
Linus Markström <lmark@kth.se>
2024-04-18 10:57:22 +0200

    test db

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ const i = pool.query('SELECT * FROM users', [1], (err, res) => {
 app.get('/get-database', async (req, res) => {
     try {
         // Logic to fetch the secret
-        res.send(i);
+        res.send(pool);
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).send('Error fetching data');
