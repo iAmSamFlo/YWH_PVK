260753e2e53140e508101d2320090edf298a325b
adamnj <adamnj@kth.se>
2024-05-09 14:26:27 +0200

    första försök till datqabasen

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -32,18 +33,39 @@ app.get('/get-secret', async (req, res) => {
 
 // Handle the button click data
 app.post('/sendData', (req, res) => {
-    const { radius, coord } = req.body;
+    console.log("hello from here");
+    const { latitude, longitude, radius} = req.body;
     
     // Save the variables as local variables in the backend
     backendRadius = radius;
-    backendCoord = coord;
+    backendlat = latitude;
+    backendlong = longitude;
 
     console.log('Received data from frontend:');
     console.log('Radius:', backendRadius);
-    console.log('Coordinates:', backendCoord);
+    console.log(backendlat);
+    console.log(backendlong);
+
 
     // Respond to the frontend if necessary
     res.send('Data received successfully!');
+
+    let db = new sqlite3.Database('./Database/databaseLite.db', sqlite3.OPEN_READWRITE, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Connected to the database.');
+    });
+ 
+    db.run(`INSERT INTO Pin(pinID, dateOfCreation, rating, message, tags, latitude, longitude, radius, userID) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`, [1, 20240101, 3, 'hello', '1,3,4', coord.latitude, coord.longitude, radius, 1], function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A pin has been inserted with rowID ${this.lastID}`);
+    });
+    db.close();
+
 });
 
 // Start the server
