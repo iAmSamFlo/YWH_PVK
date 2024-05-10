f5e20d7afc010b7902507fe89be89d7887c1beba
ELSAlinneus <elsa.linneusson@gmail.com>
2024-05-10 13:04:22 +0200

    display data for demo

---
+++ public/scripts/Pin.js
@@ -0,0 +1,8 @@
+class Pin{
+    constructor(lat, lng, radius){
+        this.latitude = lat;
+        this.longitude = lng;
+        // this.coords = {lat: lat, lng: lng};
+        this.radius = radius;
+    }
+}
